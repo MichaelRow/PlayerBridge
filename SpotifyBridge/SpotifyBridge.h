a837fd54ed721aedc2385396101a482a075185c9
Michael Row <little93@me.com>
2017-09-09 15:49:43 +0800

    add: iTunes, Spotify and Vox bridge for swift

---
+++ SpotifyBridge/SpotifyBridge.h
@@ -0,0 +1,19 @@
+//
+//  SpotifyBridge.h
+//  SpotifyBridge
+//
+//  Created by Michael Row on 2017/9/9.
+//  Copyright © 2017年 Michael Row. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SpotifyBridge.
+FOUNDATION_EXPORT double SpotifyBridgeVersionNumber;
+
+//! Project version string for SpotifyBridge.
+FOUNDATION_EXPORT const unsigned char SpotifyBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyBridge/PublicHeader.h>
+
+
---
+++ VoxBridge/VoxBridge.h
@@ -0,0 +1,19 @@
+//
+//  VoxBridge.h
+//  VoxBridge
+//
+//  Created by Michael Row on 2017/9/9.
+//  Copyright © 2017年 Michael Row. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VoxBridge.
+FOUNDATION_EXPORT double VoxBridgeVersionNumber;
+
+//! Project version string for VoxBridge.
+FOUNDATION_EXPORT const unsigned char VoxBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VoxBridge/PublicHeader.h>
+
+
