da0813e69718dc86448e2330d112a10435437e73
Michael Row <little93@me.com>
2017-09-09 15:32:50 +0800

    Initial Commit

---
+++ PlayerBridge/PlayerBridge.h
@@ -0,0 +1,19 @@
+//
+//  PlayerBridge.h
+//  PlayerBridge
+//
+//  Created by Michael Row on 2017/9/9.
+//  Copyright © 2017年 Michael Row. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlayerBridge.
+FOUNDATION_EXPORT double PlayerBridgeVersionNumber;
+
+//! Project version string for PlayerBridge.
+FOUNDATION_EXPORT const unsigned char PlayerBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerBridge/PublicHeader.h>
+
+
